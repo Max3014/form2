88bd495386525adc768d45937c276c15ec0fa52a
Max3014 <maxim3014@gmail.com>
2018-06-11 22:17:59 +0300

    Added 'hello' into response

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ let fileData = `
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain',
                           'Trailer': 'Content-MD5' });
-  res.write(fileData);
+  res.write('Hello');
   res.end();
 });
 server.on('clientError', (err, socket) => {
